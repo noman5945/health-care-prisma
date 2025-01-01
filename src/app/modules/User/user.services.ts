1da8f58dc0b67bade4a0bed6e64b83175c926727
noman5945 <abdullahnoman.5945@gmail.com>
2025-01-01 21:20:12 +0600

    [FEAT] New User and Admin create

--- src/app/modules/User/user.services.ts
+++ src/app/modules/User/user.services.ts
@@ -1,7 +1,22 @@
+import { PrismaClient, UserRole } from "@prisma/client";
+
+const prisma = new PrismaClient();
 const createAdminService = async (data: any) => {
-  return {
-    message: "Remove bra and suck tits!",
+  const userData = {
+    email: data.admin.email,
+    password: data.password,
+    role: UserRole.ADMIN,
   };
+  const result = await prisma.$transaction(async (transClient) => {
+    const createUserData = await transClient.user.create({
+      data: userData,
+    });
+    const createAdminData = await transClient.admin.create({
+      data: data.admin,
+    });
+    return createAdminData;
+  });
+  return result;
 };
 
 export const userServices = {
